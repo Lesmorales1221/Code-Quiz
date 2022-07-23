4b7cd019ca25eec9c1e09912d001f319dc5dde8f
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-07-22 22:35:53 -0400

    fixed typos in script.js

--- script.js
+++ script.js
@@ -1,88 +1,93 @@
-// Creating a quiz class
-class Quiz {
-    constructor(questions){
-        this.score = 0;
-        this.questions = questions;
-        this.questionIndex = 0;
-    }
-
-    getQuestionIndex() {
-        return this.questions[this.questionIndex];
-    }
-
-    guess(answer) {
-        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
-            this.score++;
-        }
-        this.questionIndex++;
-    }
-
-    isEnded() {
-        return this.questionIndex === this.questions.length;
-    }
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
 }
 
-//question Class
-class Question {
-    constructor(text,choices, answer) {
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer
-    }
-    isCorrectAnswer(choice) {
-        return this.answer === choice;
-    }
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
 }
 
-//Display questions
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+// Displaying the question
 function displayQuestion() {
-    if (Quiz.isEnded()) {
+    if(quiz.isEnded()) {
         showScores();
-    } else {
-    let questionElement = document.getElementById("question");
-    questionElement.innerHTML = quiz.getQuestionIndex().text;
-
-    let choices = quiz.getQuestionIndex().choices;
-    for (let i=0; i < choices.lenght; i++) {
-        let choiceElement = document.getElementById("choice" + i);
-        choiceElement.innerHTML = choices[i];
-        guess("btn" + i, choices[i]);
     }
+    else {
+        // show question
+        let questionElement = document.getElementById("question");
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
 
-    showProgress(); 
- }
+        // show options
+        let choices = quiz.getQuestionIndex().choices;
+        for(let i = 0; i < choices.length; i++) {
+            let choiceElement = document.getElementById("choice" + i);
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
 
+        showProgress();
+    }
 };
 
-//Guess answer
-function guess(id,guess) {
+function guess(id, guess) {
     let button = document.getElementById(id);
-    button.onclick = function() {
+    buttononclick = function() {
         quiz.guess(guess);
         displayQuestion();
     }
 };
 
-// Progress of the quiz
+
 function showProgress() {
     let currentQuestionNumber = quiz.questionIndex + 1;
     let ProgressElement = document.getElementById("progress");
     ProgressElement.innerHTML = 
-    'Question ${currentQuestionNumber} of ${quiz.Question.length}';
+    `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+};
 
-}
-
-//show the scores
 function showScores() {
-    let quizEndHTML = document.getElementById("quiz");
+    let quizEndHTML = 
+    `
+    <h1>Quiz Completed</h1>
+    <h2 id='score'> Your scored: ${quiz.score} of ${quiz.questions.length}</h2>
+    <div class="quiz-repeat">
+        <a href="index.html">Take Quiz Again</a>
+    </div>
+    `;
+    let quizElement = document.getElementById("quiz");
     quizElement.innerHTML = quizEndHTML;
 };
 
-// The quiz questions =>
+// create questions here
 let questions =[
     new Question(
         "Which is not a various formatting tags in HTML?",
-        ["<b>", "<em>", "<strong>", "<h1>"], "<h1>"
+        ["b", "em", "strong", "h1"], "h1"
     ),
     new Question(
         "What are the elements of the CSS Box Model?",
@@ -95,14 +100,40 @@ let questions =[
     new Question(
         "What are the uses of an embedded style sheet?"
         ["Embedded style sheet gives us the privilege to define styles in one place in an HTML document.", "Embedded style sheet is a link to images.", "Embedded style sheet is a rank that decides which style declaration has to be used to an element.", "Embedded style sheet is a property of css which allows you to display a smooth transformation between 2 or more specified colors"],"Embedded style sheet gives us the privilege to define styles in one place in an HTML document."
-    ), 
+    ),
     new Question(
         "Which HTML tag does not comein a pair?"
-        ["<br>", "<h1>", "<div>","<span>" ], "<br>"
+        ["br", "h1", "div","span" ], "br"
     )
-];
+ ];
+ 
 
-let quiz = new quiz(questions);
 
-//display the question
+// create quiz
+let quiz = new Quiz(questions);
+
+// display quiz
 displayQuestion();
+
+// Add A CountDown for the Quiz
+let time = 1;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown(){
+    let quizTimer = setInterval(function(){
+    if(quizTime <= 0) {
+        clearInterval(quizTimer);
+        showScores();
+    } else {
+        quizTime--;
+        let sec = Math.floor(quizTime % 60);
+        let min = Math.floor(quizTime / 60) % 60;
+        counting.innerHTML = `TIME: ${min} : ${sec}`;   
+    }
+},1000);
+}
+
+startCountdown();
